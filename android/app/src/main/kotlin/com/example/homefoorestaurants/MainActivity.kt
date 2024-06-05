fc0078dd909f4eebc9fa28713f20fba2f8e22713
HaripriyaKS22 <haripriyaks2522@gmail.com>
2024-06-05 09:30:49 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/homefoorestaurants/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.homefoorestaurants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/api.dart
@@ -0,0 +1,21 @@
+class api{
+
+  var base="https://9036-117-193-92-169.ngrok-free.app";
+  var restreg="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/";
+  var login="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/login/";
+
+  //category view
+  var cat="https://9036-117-193-92-169.ngrok-free.app/categories/";
+
+  //add product
+  var product="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/HOMFOO-add-products/";
+
+  //products view
+  var productview="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/HOMFOO-shop-products/";
+
+  //product update
+  var uproduct="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/HOMFOO-shop-product-update/";
+  //product update
+  var dproduct="https://9036-117-193-92-169.ngrok-free.app/HOMFOO-restaurant/HOMFOO-shop-product-delete/";
+
+}
