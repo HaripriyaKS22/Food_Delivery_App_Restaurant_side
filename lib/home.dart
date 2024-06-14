e7f779851a4df944c098b5180476aa3280848b5e
HaripriyaKS22 <haripriyaks2522@gmail.com>
2024-06-14 17:34:45 +0530

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -113,14 +113,14 @@ Future<void> _initData() async {
   Future<void> fetchProducts(String? userId) async {
 
     final token = await gettokenFromPrefs();
-    // print("8888888888888888888888888888888888${token}");
+    print("8888888888888888888888888888888888${token}");
     try {
      var response = await http.post(Uri.parse(a.productview), headers: {
       'Authorization': '$token',
     }, body: {
       'token': token,
     });
-        print("8888888888888888888888888888888888${response.body}");
+        // print("8888888888888888888888888888888888${response.body}");
 
 
       if (response.statusCode == 200) {
